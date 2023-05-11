79bc9c273505623e7c2c3da107a010a0ec4fda50
andanizr <133138241+andanizr@users.noreply.github.com>
2023-05-11 12:27:32 +0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,36 @@
+public class App {
+    
+    public static void main(String[] args) throws Exception{
+        Hero Hr = new Hero();
+        Enemy En = new Enemy();
+        Character Fg = new Fighter();
+        Character Wt = new Witch();
+        Character Hr2 = new Hero();
+        Character En2 = new Enemy();
+
+        Hr.move();
+        Hr.move("maju");
+        Hr = new Fighter();
+        System.out.println("====================");
+        Hr.move();
+        System.out.println("====================");
+        En.move();
+        En.move(3);
+        System.out.println("====================");
+        En = new Witch();
+        En.move();
+        System.out.println("====================");
+        Fg.move();
+        Wt.move();
+        Hr2.move();
+        En2.move();
+        System.out.println("====================");
+        
+        
+        Character cr = new Witch();
+        cr.move();
+        Fighter f = (Fighter) cr;
+        f.move();
+
+    }
+}
